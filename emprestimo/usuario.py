0c0ccf367e6aa895e05e04f76a9e09a43fee84fd
lordefps <gabrielbotaoap@gmail.com>
2021-06-21 18:05:09 -0300

    Criando dict para usuario

---
+++ emprestimo/usuario.py
@@ -0,0 +1,43 @@
+# TODO:Essa classe serve como exemplo uma conexao com o DB
+#      e cadastro de clientes
+
+class user():
+    def user_1():
+        dict_user1 = {
+            "nome" : "Jorge",
+            "sobrenome" : "Silva",
+            "saldo" : +1000
+        }
+        return dict_user1
+    def user_2():
+        dict_user2 = {
+            "nome" : "Sergio",
+            "sobrenome" : "Silva",
+            "saldo" : -1000
+        }
+        return dict_user2
+
+    def user_3():
+        dict_user3 = {
+            "nome" : "Rogerio",
+            "sobrenome" : "Silva",
+            "saldo" : +10000
+        }
+        return dict_user3
+
+    def user_4():
+        dict_user4 = {
+            "nome" : "Ricardo",
+            "sobrenome" : "Silva",
+            "saldo" : -100
+        }
+        return dict_user4
+    
+    # def user_full():
+    #     dict_userfull = {
+    #         "user1" : user.user_1["nome"],
+    #         "user2" : user.user_2["nome"],
+    #         "user3" : user.user_3["nome"],
+    #         "user4" : user.user_4["nome"]
+    #     }
+
