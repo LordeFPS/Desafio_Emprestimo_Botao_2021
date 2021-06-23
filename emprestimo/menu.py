2671414a0759e84228edde38c923dcc61ef28651
lordefps <gabrielbotaoap@gmail.com>
2021-06-23 04:04:01 -0300

    Atualizando Menus

--- emprestimo/menu.py
+++ emprestimo/menu.py
@@ -11,7 +11,7 @@ class menu():
         *******************************
         **        Emprestimos        **
         *******************************
-        ** 1 - {dtemp.total_emprestimo.emprestimo_1()["nome"]}     **
+        ** 1 - Opção Personalizada   **
         ** 5 - Voltar                **
         *******************************
         ''')
@@ -20,7 +20,7 @@ class menu():
         *******************************
         **        Emprestimos        **
         *******************************
-        ** 1 - {dtemp.total_emprestimo.emprestimo_1()["nome"]}     **
+        ** 1 - Opção Personalizada   **
         ** 2 - {dtemp.total_emprestimo.emprestimo_2()["nome"]}     **
         ** 5 - Voltar                **
         *******************************
@@ -31,9 +31,9 @@ class menu():
         *******************************
         **        Emprestimos        **
         *******************************
-        ** 1 - {dtemp.total_emprestimo.emprestimo_1()["nome"]}     **
+        ** 1 - Opção Personalizada   **
         ** 2 - {dtemp.total_emprestimo.emprestimo_2()["nome"]}     **
-        ** 3 - {dtemp.total_emprestimo.emprestimo_3()["nome"]}     **
+        ** 3 - {dtemp.total_emprestimo.emprestimo_3()["nome"]}    **
         ** 5 - Voltar                **
         *******************************
         ''')
@@ -43,26 +43,24 @@ class menu():
         *******************************
         **        Emprestimos        **
         *******************************
-        ** 1 - {dtemp.total_emprestimo.emprestimo_1()["nome"]}     **
+        ** 1 - Opção Personalizada   **
         ** 2 - {dtemp.total_emprestimo.emprestimo_2()["nome"]}     **
-        ** 3 - {dtemp.total_emprestimo.emprestimo_3()["nome"]}     **
-        ** 4 - {dtemp.total_emprestimo.emprestimo_4()["nome"]}     **
+        ** 3 - {dtemp.total_emprestimo.emprestimo_3()["nome"]}    **
+        ** 4 - {dtemp.total_emprestimo.emprestimo_4()["nome"]}    **
         ** 5 - Voltar                **
         *******************************
         ''')
     
     def menu_user():
         print(f'''
-        **********************
-        **     Usuários     **
-        **********************
-        ** 1 - {usr.user.user_1()["nome"]}        **
-        ** 2 - {usr.user.user_2()["nome"]}       **
-        ** 3 - {usr.user.user_3()["nome"]}      **
-        ** 4 - {usr.user.user_4()["nome"]}      **
-        ** 5 - Sair         **
-        **********************
+        ******************************
+        **   Clientes Cadastrados   **
+        ******************************
+        ** 1 - {usr.user.user_1()["nome"]} {usr.user.user_1()["sobrenome"]}         **
+        ** 2 - {usr.user.user_2()["nome"]} {usr.user.user_2()["sobrenome"]}        **
+        ** 3 - {usr.user.user_3()["nome"]} {usr.user.user_3()["sobrenome"]}      **
+        ** 4 - {usr.user.user_4()["nome"]} {usr.user.user_4()["sobrenome"]}    **
+        ** 5 - Sair                 **
+        ******************************
         ''')
-    
-
 
