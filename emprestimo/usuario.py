5d9d4346646f825026efdb91dfb1ee45bb079753
lordefps <gabrielbotaoap@gmail.com>
2021-06-23 04:01:55 -0300

    Usando método Score

--- emprestimo/usuario.py
+++ emprestimo/usuario.py
@@ -3,31 +3,31 @@ class user():
     def user_1():
         dict_user1 = {
             "nome" : "Jorge",
-            "sobrenome" : "Silva",
-            "saldo" : +1000
+            "sobrenome" : "Stugan",
+            "score" : 0
         }
         return dict_user1
     def user_2():
         dict_user2 = {
             "nome" : "Sergio",
-            "sobrenome" : "Silva",
-            "saldo" : -1000
+            "sobrenome" : "Bottas",
+            "score" : 300
         }
         return dict_user2
 
     def user_3():
         dict_user3 = {
             "nome" : "Rogerio",
-            "sobrenome" : "Silva",
-            "saldo" : +10000
+            "sobrenome" : "Leclerc",
+            "score" : 650
         }
         return dict_user3
 
     def user_4():
         dict_user4 = {
             "nome" : "Ricardo",
-            "sobrenome" : "Silva",
-            "saldo" : -100
+            "sobrenome" : "Verstapen",
+            "score" : 950
         }
         return dict_user4
     
